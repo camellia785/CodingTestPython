7239b9ffc3bf649e3d3e9507cf5635e95f5b9a82
YEONA <79030514+camellia785@users.noreply.github.com>
2025-09-22 21:19:15 -0500

    [level 0] Title: 이어 붙인 수, Time: 0.03 ms, Memory: 9.43 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,11 @@
+def solution(num_list):
+    odd_sum = ""
+    even_sum = "" # 문자열
+    
+    for i in range(len(num_list)):
+        if num_list[i]%2==1:
+            odd_sum += str(num_list[i])
+        else:
+            even_sum += str(num_list[i])
+    
+    return int(odd_sum) + int(even_sum)
