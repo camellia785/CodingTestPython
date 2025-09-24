7f3d1ca314da15aafacc0a19b41022d0a21fdffd
YEONA <79030514+camellia785@users.noreply.github.com>
2025-09-24 10:19:04 -0500

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.03 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -1,12 +1,10 @@
 def solution(n):
-    result = []
-    result.append(n)
+    result = [n]
     while n != 1:
         if n%2==0:
-            n = n/2
-            result.append(n)
+            n = n//2
         else:
             n = 3*n+1
-            result.append(n)
+        result.append(n)
                 
     return result
