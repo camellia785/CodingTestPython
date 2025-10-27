1ded646b761fd46f6fa788ef62c1e3052a3102df
YEONA <79030514+camellia785@users.noreply.github.com>
2025-10-27 00:29:16 -0500

    [level 0] Title: 점의 위치 구하기, Time: 0.00 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.py
@@ -1,12 +1,6 @@
 def solution(dot):
     x, y = dot[0], dot[1]
     if x > 0:
-        if y >0:
-            return 1
-        else:
-            return 4
+        return 1 if y >0 else 4
     elif x<0:
-        if y >0:
-            return 2
-        else:
-            return 3
+        return 2 if y >0 else 3
