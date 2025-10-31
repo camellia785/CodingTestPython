ecc1ff07dbd6b846e12362c67341fdf6b2fcf0ef
YEONA <79030514+camellia785@users.noreply.github.com>
2025-10-31 11:33:05 -0500

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.py
@@ -1,7 +1,3 @@
 def solution(sides):
-    temp = sides[:]
-    temp.remove(min(sides))
-    temp.remove(max(sides))
-    
-    mid = temp[0]
-    return 1 if min(sides) + mid > max(sides) else 2
+    answer = 0
+    return answer
