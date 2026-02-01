226709a18eac75754935fa2032c6e2d68193deca
YEONA <79030514+camellia785@users.noreply.github.com>
2026-02-01 14:43:44 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.04 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -5,3 +5,9 @@ def solution(strings, n):
     strings.sort(key=sort_logic)
     
     return strings
+
+# 방법2 -> 함수 두개 정의하는 것 대신에, 일회용함수 람다 사용
+# def solution(strings, n):
+#     # sort_logic 함수 자리에 람다를 직접 집어넣기
+#     strings.sort(key=lambda x: (x[n], x))
+#     return strings
