ac328308c9d10c98810db33032cee56f67b3d634
YEONA <79030514+camellia785@users.noreply.github.com>
2026-02-06 20:41:45 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.40 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,13 +1,12 @@
 from collections import Counter
 
 def solution(participant, completion):
-    # {키:밸류, 키:밸류} 로 저장되는 dict 형태
-    p_count = Counter(participant)
-    c_count = Counter(completion)
-    
+    p_count = Counter(participant) # Counter는 타입의 이름을 말하는 것
+    c_count = Counter(completion)  # {키:밸류, 키:밸류} 로 저장되는 dict 형태
     answer = p_count - c_count
+    
+    print(answer.keys()) # dict_keys(['vinko'])
+    print(list(answer.keys())) # ['vinko']
     
     # 딕셔너리에서 키 값만 모은 뭉치 -> 리스트 강제변환 -> 첫값
     return list(answer.keys())[0]
-    
-    
