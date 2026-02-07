c97cf7bf4a69d48b348ccc8e50642f99b3b02ecf
YEONA <79030514+camellia785@users.noreply.github.com>
2026-02-08 01:20:35 +0900

    [level 1] Title: 옹알이 (2), Time: 0.16 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -1,25 +1,9 @@
 def solution(babbling):
     answer = 0
-    sounds = ["aya", "ye", "woo", "ma" ]
-    
-    for bab in babbling:
-        valid = True #초기화
-        
-        # 1) 같은 발음이 연속되면 탈락
-        for s in sounds:
-            if s*2 in bab:
-                valid = False
-                break
-        
-        if not valid:
-            continue # valid가 True인 경우만
-        
-        # 2) 허용된 발음을 공백으로 치환 -> ""이면 발음가능
-        temp = bab # temp에 bab 임시저장
-        for s in sounds:
-            temp = temp.replace(s, " ")
-            
-        if temp.strip() == "":
-            answer += 1
-
+    for i in babbling:
+        for j in ['aya','ye','woo','ma']:
+            if j*2 not in i:
+                i=i.replace(j,' ')
+        if len(i.strip())==0:
+            answer +=1
     return answer
